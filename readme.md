ea742eab4629b94a872beaa8cb8e42d14f669833
Ade Yusuf <adeyusuf005@gmail.com>
2022-01-01 17:19:18 +0700

    Membuat website menjadi responsive

---
+++ readme.md
@@ -0,0 +1,6 @@
+# TRAVELINDO WEB TRAVEL
+
+Ini adalah sebuah desain website untuk sebuah platform travel yang bernama _travelindo._ dan dibuat menggunakan teknologi:
+`html`
+`css`
+`javascript`
--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const nav = document.querySelector('nav');
 
+window.addEventListener('scroll', function() {
+    nav.classList.toggle('fixed', scrollY > 500)
+});
+
+const search = document.getElementById('search');
+const modalSearch = document.querySelector('.modal-search');
+const closeModal  = document.querySelector('.search-title span');
+
+search.addEventListener('click', function(e) {
+    e.preventDefault();
+    modalSearch.classList.add('show');
+});
+
+closeModal.addEventListener('click', function() {
+    modalSearch.classList.remove('show');
+});
